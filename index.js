0b74c87983547b318cc4c964d9edf2f3e8de40f0
unknown <alvin.aleguiojo@gmail.com>
2025-01-21 23:48:30 +0800

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import axios from 'axios';
 import { accounts } from './const.js';
-const cors = require("cors");
+import cors from 'cors'
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 
 // const symbol = "GBPUSDm";
 // const operation = "Buy";
